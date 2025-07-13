75b96e149432d5a6e49026bcdbdc36e4fa74d1f2
seok0205 <101177749+seok0205@users.noreply.github.com>
2025-07-13 17:07:07 +0900

    feat: community, tourinfo 앱 생성

--- backend/settings.py
+++ backend/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'community',
     'run',
+    'tourinfo',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
